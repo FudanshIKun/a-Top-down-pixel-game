3125e983016b84ff7114e77ef53900f977a81625
NOTMOE <n0tm0e.work@gmail.com>
2021-12-21 22:14:49 +0700

    Fix PlayerMovement.cs
    ยังติดปัญหาเรื่องการเดินอยู่นิดหน่อย แต่พน.จะมาแก้แต่เช้า รีบนอนก่อน - ^ -

--- Prototype/Assets/Scripts/PlayerMovements.cs
+++ Prototype/Assets/Scripts/PlayerMovements.cs
@@ -12,17 +12,43 @@ public class PlayerMovements : MonoBehaviour
         
     }
 
-    
+
     private void Update()
     {
+        // Reset Vector2
+        Vector2 movement = Vector2.zero;
+
         // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float x = movement.x = Input.GetAxisRaw("Horizontal");
+        float y = movement.y = Input.GetAxisRaw("Vertical");
+
+        // Detect direction of animation or sprite
+        if (x == -1)
+        {
+            
+        }
+        else if (x == 1)
+        {
+            
+        }
+
+        if (y == -1)
+        {
+            
+        }
+        else if (y == 1)
+        {
+            
+        }
+
+        movement.Normalize();
+        Debug.Log(x);
+        Debug.Log(y);
     }
     private void FixedUpdate()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
+        // Moving
+        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
 
+    }
 }
