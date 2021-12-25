df34265937d710b1a3159e5b7f5d4beb4ad7db45
NOTMOE <n0tm0e.work@gmail.com>
2021-12-25 11:45:46 +0700

    Nothing
    ไม่มีอะไรหรอกกกกกกกก

--- Prototype/Assets/Scripts/TilemapDetection.cs
+++ Prototype/Assets/Scripts/TilemapDetection.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StairLayerTriggers : MonoBehaviour
+public class TilemapDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        if (hit.collider != null)
+        {
+            Debug.Log("Raycast is working");
+        }
     }
 }
