67bc25ce59182cfede47082a20cdf6299dc30236
NOTMOE <n0tm0e.work@gmail.com>
2021-12-28 17:15:31 +0700

    Improve Camera system by create border

--- Prototype/Assets/Scripts/CameraMovement.cs
+++ Prototype/Assets/Scripts/CameraMovement.cs
@@ -6,15 +6,20 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public float lerpSpeed = 2.0f;
-    
+    public Vector3 minValues, maxValues;
 
     void Update()
     {
         if(target == null) return;
         Vector3 targetPos;
 
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(target.position.x, minValues.x, maxValues.x),
+            Mathf.Clamp(target.position.y, minValues.y, maxValues.y),
+            Mathf.Clamp(target.position.z, minValues.z, maxValues.z));
+
         targetPos = new Vector3(target.position.x, target.position.y, -100);
-        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, boundPosition, lerpSpeed * Time.deltaTime);
 
         
     }
